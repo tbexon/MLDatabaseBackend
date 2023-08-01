beba2cfed7c6c8428b019d153805bd76873dbf3a
tbexon <60880736+tbexon@users.noreply.github.com>
2023-08-01 12:12:11 +0100

    Added Column name lists

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ fixture_name_fld = 'InstType'
 wattage_fld = 'Wattage'
 weight_fld = 'Weight'
 
+
 # Manufactere Table Column Names
 manf_ID_fld = 'ManufacturerID'
 manufacturer_fld = 'Manufacturer'
@@ -31,8 +32,37 @@ add_fld = "add_grp"
 view_fld = "view_grp"
 manufacturer_grp_fld = "manufacturer_grp"
 
+
 # DMX Profile Table Column Names
 DMXprofID_fld = "DMXProfID"
 prof_name = "profile_name"
 dmxcnt_fld = "DMX_count"
+
+# COLUMN LISTS
+
+# List of Fixture Table Columns Names
+fixture_col_names = [fixture_ID_fld,
+             fixture_name_fld,
+             manf_ID_fld,
+             wattage_fld,
+             weight_fld,
+             userID_fld
+             ]
+
+# List of Manufacturer Table Columns Names
+manf_col_names = [manf_ID_fld,
+             manufacturer_fld,
+             userID_fld
+             ]
+
+# List of User Column names
+user_col_names = [userID_fld,
+             username_fld,
+             email_fld,
+             password_fld,
+             admin_fld,
+             add_fld,
+             edit_fld,
+             view_fld,
+             manufacturer_grp_fld]
 
