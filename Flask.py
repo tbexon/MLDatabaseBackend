a4827cf6a2c49f0f9f6b6a95be31ccbc83608a63
tbexon <60880736+tbexon@users.noreply.github.com>
2023-08-03 16:11:37 +0100

    If no ID specified returns whole fixture table

--- Flask.py
+++ Flask.py
@@ -1,6 +1,6 @@
 from flask import Flask,jsonify, request
 import config as cfg
-from Database import GetFixtureByID
+from Database import GetFixtureByID, GetAllFixtures
 
 
 app = Flask(__name__)  # Create Flask Server
@@ -10,8 +10,14 @@ def GetFixture():
     print("API Call Received!")
     params = request.args.to_dict()  # Converts params from GET request to dict
 
-    fix_ID = params[cfg.fixture_ID_fld]
-    fix_data_dict = GetFixtureByID(fix_ID)
+    fix_ID = request.args.get(cfg.fixture_ID_fld)  # Attempts to get the fixture ID if it has been included in request
+    if fix_ID:
+        # If a specific fixture ID has been specified in request URL
+        fix_data_dict = GetFixtureByID(fix_ID)  # Gets the individual fixture Row based on Fix ID
+    else:
+        # If no fixture ID has been specified
+        fix_data_dict = GetAllFixtures()  # Gets all fixtures in Fixture Table
+    print(f"Final Fix Data: {fix_data_dict}")
     return jsonify(fix_data_dict)
 
 
